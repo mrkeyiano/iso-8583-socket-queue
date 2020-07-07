705c5e68a99e468d3c330ff09358050a7a977c39
Femi <iamkeyiano@gmail.com>
2020-07-07 16:05:04 +0100

    add shell

--- server.sh
+++ server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-node socketQueue.js --upstreamHost=10.154.0.12 --upstreamPort=43666 --listenPort=43666
+node socketQueue.js --upstreamHost=10.154.0.12 --upstreamPort=43666 --listenPort=43666 --vv --logFile=log.txt
